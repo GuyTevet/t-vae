ba455ccd8e719a340deaf2f3ca8f8f177911e0de
GuyTevet <guy.tvt@gmail.com>
2021-05-03 10:53:09 +0300

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='t-vae',
+    version='0.1',
+    packages=['tvae', 'tvae.nn', 'tvae.nn.modules', 'tvae.data', 'tvae.models', 'tests', 'tests.data', 'tests.models'],
+    url='',
+    license='',
+    author='guytevet',
+    author_email='',
+    description=''
+)
